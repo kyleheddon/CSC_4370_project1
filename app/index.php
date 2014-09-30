7bfb1127ca8414db6866fcd3e1925307deed31a4
Kyle Heddon <ktheddon@gmail.com>
2014-09-30 18:37:24 -0400

    first commit

---
+++ app/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Project 1</title>
+		<link href="css/main.css" rel="stylesheet" type="text/css" />
+		<meta content="initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" name="viewport">
+	</head>
+	<body>
+		<h1>Project 1</h1>
+	</body>
+</html>
---
+++ sync.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+
+`cd app && scp -r ./ csc4370FA14_59@codd.cs.gsu.edu:~/public_html/`
