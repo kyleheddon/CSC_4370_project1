fa7870a5a4a54aabf7dc3baa097d4a8667debcae
Kyle Heddon <ktheddon@gmail.com>
2014-10-01 19:03:34 -0400

    setup

--- app/index.php
+++ app/index.php
@@ -6,6 +6,24 @@
 		<meta content="initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" name="viewport">
 	</head>
 	<body>
-		<h1>Project 1</h1>
+		<h1>Tic Tac Toe</h1>
+		<h2>X's Turn - hello</h2>
+		<form>
+			<table id="board">
+<?php for($i = 0; $i < 3; $i++){ ?>
+				<tr>
+	<?php for($j = 0; $j < 3; $j++){ ?>
+					<td class="x">
+						<label>
+							<input name="cell" value="<?php echo "$i$j"; ?>" type="radio" />
+							<div class="mask">X</div>
+						</label>
+					</td>
+	<?php } ?>
+				</tr>
+<?php } ?>
+			</table>
+			<input type="submit" value="Submit" />
+		</form>
 	</body>
 </html>
--- sync.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env ruby
-
-`cd app && scp -r ./ csc4370FA14_59@codd.cs.gsu.edu:~/public_html/`
